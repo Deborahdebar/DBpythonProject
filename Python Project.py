18cc2477489be5ddbb24341803a6377b4cc8b30f
deborahbarrett <deborahbarrett@hotmail.com>
2021-04-15 20:28:33 +0100

    seconnd commit

--- Python Project.py
+++ Python Project.py
@@ -15,4 +15,36 @@ covid_vac_prog = pd.read_csv("/Users/deborahbarrett/Downloads/country_vaccinatio
 print(len(covid_vac_prog))
 
 ## To check the first 5 rows
+## Once run I can see Afghanistan is the first country mentioned in the dataset
 print(covid_vac_prog.head())
+
+## To check the last 5 rows
+## I can see that Zimbabwe
+print(covid_vac_prog.tail())
+
+## Find how many rows and columns
+print(covid_vac_prog.shape)
+
+## loc refers to the label index
+## .iloc refers to the positional index
+## Check cols date, country and daily_vaccinatons and check positional index 1 and the last index -1
+index_check = covid_vac_prog[["date", "country", "daily_vaccinations"]]
+print(index_check.iloc[[1, -1]])
+
+#To check the first row, i.e. index 0.
+print(index_check.iloc[[0]])
+
+## get first two elements, as seen below index 0 and 1.
+print(covid_vac_prog[0:2])
+
+## Check positional index 0 and last row
+print(index_check.iloc[[0, -1]])
+
+## Get an overview of some summary stats
+print(covid_vac_prog.describe())
+
+## Get to know the names of the columns to see if all are needed
+print(covid_vac_prog.columns)
+
+## Certain cols are not needed and can be dropped - I decided that I would not need source name and source website
+print(covid_vac_prog.drop(['source_name','source_website'], inplace=True, axis=1))
